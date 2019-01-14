06a778825c071143c15271de808d9cee2fa6e399
Iliyan Stoyanov <virus_22@abv.bg>
2019-01-14 14:37:52 +0200

    Updated home page and created all other pages for the version.

--- SemmaManagement/SemmaManagement/Scripts/homeGallery.js
+++ SemmaManagement/SemmaManagement/Scripts/homeGallery.js
@@ -3,20 +3,30 @@
 var images = document.querySelectorAll('.photo img');
 var i = 0;
 
-btn_prev.onclick = function(){
-   images[i].className = "";
-    i = i - 1;
-    if( i < 0){
-      i = images.length - 1;  
-    }
-    images[i].className = "shown";
-};
-
-btn_next.onclick = function(){
+setInterval(function () {
     images[i].className = "";
     i = i + 1; //i++
-    if( i >= images.length){
-      i = 0;  
+    if (i >= images.length) {
+        i = 0;
     }
     images[i].className = "shown";
-};
+}, 4000);
+
+//btn_prev.onclick = function(){
+//   images[i].className = "";
+//    i = i - 1;
+//    if( i < 0){
+//      i = images.length - 1;  
+//    }
+//    images[i].className = "shown";
+//};
+
+//btn_next.onclick = function(){
+//    images[i].className = "";
+//    i = i + 1; //i++
+//    if( i >= images.length){
+//      i = 0;  
+//    }
+//    images[i].className = "shown";
+//};
+
